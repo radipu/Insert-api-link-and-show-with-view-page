f53ea6944e9ab47a89cc5882073e4f975dc112b1
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2021-07-30 09:58:37 +0600

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ITGROWTEST_NEW.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ITGROWTEST_NEW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var newsArticles = GetArticles();
+            return View(newsArticles);
+        }
+
+        protected DataContainer GetArticles()
+        {
+            const string apiKey = "x-api-key";
+            const string clientKey = "7cb1cc4c775e4bb3b4765a0cae5c6c06";
+            const string url = "https://newsapi.org/v2/everything?q=COVID&from=2021-07-13";
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Headers.Add(apiKey, clientKey);
+
+            string jsonResponse;
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                var dataStream = response.GetResponseStream();
+                var reader = new StreamReader(dataStream);
+                jsonResponse = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+            }
+
+            var finalResults = JsonConvert.DeserializeObject<DataContainer>(jsonResponse);
+            return finalResults;
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITGROWTEST_NEW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
